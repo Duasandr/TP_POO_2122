acfb3130575657fffa3da36d69172bdb8cb9c79f
JoelBarros24 <a94692@alunos.uminho.pt>
2022-04-12 18:44:45 +0100

    Terminei a classe SmartBulb.

--- src/com/grupo/device/SmartBulb.java
+++ src/com/grupo/device/SmartBulb.java
@@ -2,13 +2,15 @@ package com.grupo.device;
 
 import java.util.Objects;
 
+/**
+ * Class SmartBulb
+ */
 public class SmartBulb extends SmartDevice{
     //Variáveis de instância
     private int tonalidade;
     private Double dimensao;
 
     //Construtores
-
     /**
      * Construtor vazio
      */
@@ -19,14 +21,14 @@ public class SmartBulb extends SmartDevice{
     /**
      * Construtor por parâmetros
      *
-     * @param id
-     * @param estado
-     * @param potencia
-     * @param preco_instalacao
-     * @param tone
-     * @param dimensao
+     * @param id Identificador do fabricante.
+     * @param estado Estado atual do aparelho (ligado - desligado).
+     * @param potencia Potência de consumo do aparelho.
+     * @param preco_instalacao Preço de instalação definido pelo fabricante.
+     * @param tone Tipo de luz selecionada.
+     * @param dimensao Tamanho do aparelho.
      */
-    protected SmartBulb(String id , boolean estado , int potencia , double preco_instalacao , int tone , double dimensao){
+    protected SmartBulb(String id , boolean estado , int potencia , Double preco_instalacao , int tone , Double dimensao){
         this.setIdFabricante(id);
         this.setEstado(estado);
         this.setPotencia(potencia);
@@ -35,43 +37,78 @@ public class SmartBulb extends SmartDevice{
         this.setDimensao(dimensao);
     }
 
+    /**
+     * Construtor por cópia.
+     * @param bulb Aparelho a ser copiado.
+     */
     protected SmartBulb(SmartBulb bulb){
         this(bulb.getIdFabricante(), bulb.getEstado(), bulb.getPotencia(), bulb.getPrecoInstalacao(), bulb.getTonalidade(), bulb.getDimensao());
     }
 
     //Métodos de instância
 
+    //Getters
+
+    /**
+     * Devolve a intensidade da luz do aparelho.
+     * @return Tipo de luz.
+     */
     public int getTonalidade(){
         return this.tonalidade;
     }
 
+    /**
+     * Devolve as dimensões do aparelho.
+     * @return Dimensões do aparelho.
+     */
     public double getDimensao(){
         return this.dimensao;
     }
 
+    /**
+     * Define a intensidade da luz no aparelho.
+     * @param tone Intensidade da luz.
+     */
     public void setTonalidade(int tone){
         this.tonalidade = tone;
     }
 
-    protected void setDimensao(double dimensao){
+    /**
+     * Define as dimensões do aparelho.
+     * @param dimensao Dimensões do aparelho.
+     */
+    protected void setDimensao(Double dimensao){
         this.dimensao = dimensao;
     }
 
+    /**
+     * Devolve o consumo de energia do aparelho.
+     * @return Energia consumida pelo aparelho.
+     */
     @Override
     public Double consumoEnergia() {
         if (this.getEstado()) {
-            if (this.tonalidade >= 0 && this.tonalidade < 5) return (Double) (getPotencia() * 0.2);
-            if (this.tonalidade > 5 && this.tonalidade < 10) return (Double) (getPotencia() * 0.4);
-            if (this.tonalidade > 10 && this.tonalidade < 15) return (Double) (getPotencia() * 0.5);
+            if (this.tonalidade >= 0 && this.tonalidade < 5) return getPotencia() * 0.2;
+            if (this.tonalidade > 5 && this.tonalidade <= 10) return getPotencia() * 0.4;
+            if (this.tonalidade > 10 && this.tonalidade < 15) return getPotencia() * 0.5;
         }
         return null;
     }
 
+    /**
+     * Clona o aparelho.
+     * @return Aparelho clonado.
+     */
     @Override
     public SmartBulb clone() {
         return new SmartBulb(this);
     }
 
+    /**
+     * Verifica se dois objetos são iguais.
+     * @param o Objeto a comparar.
+     * @return Verdadeiro ou Falso.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -85,11 +122,23 @@ public class SmartBulb extends SmartDevice{
                 this.getDimensao() == bulb.getDimensao();
     }
 
+    /**
+     * Devolve uma representação textual do objeto.
+     * @return Representação do objeto.
+     */
     @Override
     public String toString(){
-        return null;
+        return super.toString() + "{ Tonalidade: " + this.getTonalidade() +
+                ", Dimensão: " + this.getDimensao() +
+                "}";
     }
 
+    /**
+     * Cria um indice através de uma função de hash.
+     * @return Indice
+     */
     @Override
-    public int hashCode(){return 0;}
+    public int hashCode() {
+        return Objects.hash(this.getIdFabricante(), this.getPotencia(), this.getPrecoInstalacao());
+    }
 }
